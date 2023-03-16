7bb44a913646dcecee936fb375f7118fc82a6eaf
Yeasin Kabir Joy <52117661+YeasinKabirJoy@users.noreply.github.com>
2023-03-16 10:21:24 +0600

    Login Register with class based Views

--- user/urls.py
+++ user/urls.py
@@ -3,8 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home,name='home'),
-    path("registration", views.register_user,name='registration'),
-    path("login", views.login_user,name='login'),
-    path("logout", views.logout_user,name='logout'),
+    path("", views.Home.as_view(),name='home'),
+    path("registration", views.RegisterUser.as_view(),name='registration'),
+    path("login", views.LoginUser.as_view(),name='login'),
+    path("logout", views.Logout.as_view(),name='logout'),
+    # path("", views.home,name='home'),
+    # path("registration", views.register_user,name='registration'),
+    # path("login", views.login_user,name='login'),
+    # path("logout", views.logout_user,name='logout'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import login,authenticate,logout
+from django.views import View
 from .forms import RegistrationForm
 from .models import User
 
-def home(request):
-    return render(request,'home.html')
+# def home(request):
+#     return render(request,'home.html')
 
-def register_user(request):
-    form = RegistrationForm()
+class Home(View):
+    def get(self,request):
+        return render(request,'home.html')
+class RegisterUser(View):
+    def get(self,request):
+        form = RegistrationForm()
+        context={
+        'form':form
+        }
+        return render(request,'register.html',context)
 
-    if request.method == 'POST':
+    def post(self,request):
         form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
@@ -18,18 +27,37 @@ def register_user(request):
             user = authenticate(request,email=email,password=password)
             login(request,user)
             return redirect(request.GET['next'] if 'next' in request.GET else 'home')
-    context={
-        'form':form
-    }
-    return render(request,'register.html',context)
+        context={
+            'form':form
+        }
+        return render(request,'register.html',context)
 
-def login_user(request):
-    if request.user.is_authenticated:
-        return redirect('home')
-    if request.method == "POST":
+# def register_user(request):
+#     form = RegistrationForm()
+
+#     if request.method == 'POST':
+#         form = RegistrationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             email = form.cleaned_data.get('email')
+#             password = form.cleaned_data.get('password1')
+#             user = authenticate(request,email=email,password=password)
+#             login(request,user)
+#             return redirect(request.GET['next'] if 'next' in request.GET else 'home')
+#     context={
+#         'form':form
+#     }
+#     return render(request,'register.html',context)
+
+class LoginUser(View):
+    def get(self,request):
+        if request.user.is_authenticated:
+            return redirect('home')
+        return render(request,'login.html')
+    
+    def post(self,request):
         email = request.POST['email']
         password = request.POST['password']
-        print(email,password)
         try:
             user = User.objects.get(email=email)
         except User.DoesNotExist:
@@ -39,8 +67,31 @@ def login_user(request):
         if user is not None:
             login(request,user)
             return redirect(request.GET['next'] if 'next' in request.GET else 'home')
-    return render(request,'login.html')
+        return render(request,'login.html')
+    
 
-def logout_user(request):
-    logout(request)
-    return redirect('login')
+
+# def login_user(request):
+#     if request.user.is_authenticated:
+#         return redirect('home')
+#     if request.method == "POST":
+    #     email = request.POST['email']
+    #     password = request.POST['password']
+    #     try:
+    #         user = User.objects.get(email=email)
+    #     except User.DoesNotExist:
+    #         return redirect('login')
+    #     user = authenticate(request,email=email,password=password)
+
+    #     if user is not None:
+    #         login(request,user)
+    #         return redirect(request.GET['next'] if 'next' in request.GET else 'home')
+    # return render(request,'login.html')
+
+class Logout(View):
+    def get(self,request):
+        logout(request)
+        return redirect('login')
+# def logout_user(request):
+#     logout(request)
+#     return redirect('login')
